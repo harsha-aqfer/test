047d7b53ebad1fcaa252cbc31fadafa41ea1928c
harsha@startsmartlabs.com <harsha@startsmartlabs.com>
2022-10-08 16:07:47 +0530

    Deleted 'test234' job in sandbox 'default' of cid 'c008'

--- datalake/cids/c008/sandboxes/default/jobs/test234/user/transform.py
+++
@@ -1,9 +0,0 @@
-import schema
-
-def dat_hnd(rec: any) -> any:
-    print("*************************")
-    print(rec)
-    return [rec]
-
-def schema_handler_v1(sch: schema.Schema) -> (schema.Schema, any):
-    return sch, 
